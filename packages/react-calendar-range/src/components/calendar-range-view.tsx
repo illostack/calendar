8ee9015083557de9e17ca1d65f998221476e9b34
illodev <ajp2010@hotmail.es>
2025-03-09 23:24:50 +0100

    refactor: replace CalendarRangeView with CalendarDaysViewTemplate for improved structure

--- packages/react-calendar-range/src/components/calendar-range-view.tsx
+++ packages/react-calendar-range/src/components/calendar-range-view.tsx
@@ -1,73 +1,7 @@
 "use client";
 
-import {
-  CalendarContextMenu,
-  CalendarTimeIndicator,
-  CalendarView,
-  addDays,
-  getRangeDays,
-  mergeRefs,
-  useCalendar
-} from "@illostack/react-calendar";
-import {
-  CalendarDayContent,
-  CalendarDayDndOverlay,
-  CalendarDayDndProvider,
-  useCalendarDayInteraction,
-  useCalendarDayResize,
-  useCalendarDaySelection
-} from "@illostack/react-calendar-day";
-import * as React from "react";
-
-import { CalendarRangeAxis } from "./calendar-range-axis";
-import { CalendarRangeHeader } from "./calendar-range-header";
-
-interface CalendarRangeViewProps extends React.HTMLAttributes<HTMLDivElement> {
-  className?: string;
-}
-
-const CalendarRangeView = React.forwardRef<
-  HTMLDivElement,
-  CalendarRangeViewProps
->((props, ref) => {
-  const calendar = useCalendar();
-  const dates = calendar.useWatch((s) => s.dates);
-  const selectionRef = useCalendarDaySelection();
-  const resizeRef = useCalendarDayResize();
-  const interactionRef = useCalendarDayInteraction();
-
-  const { calendarHeight } = calendar.getLayout();
-
-  return (
-    <div ref={ref} {...props}>
-      <CalendarRangeHeader />
-      <div
-        className="relative h-full select-none pl-20"
-        style={{
-          height: calendarHeight
-        }}
-      >
-        <CalendarRangeAxis />
-        <CalendarTimeIndicator />
-        <CalendarDayDndProvider>
-          <CalendarContextMenu>
-            <div
-              ref={mergeRefs(selectionRef, resizeRef, interactionRef)}
-              className="relative grid h-full w-full"
-              style={{ gridTemplateColumns: `repeat(${dates.length}, 1fr)` }}
-            >
-              {dates.map(({ date }, index) => (
-                <CalendarDayContent key={index} date={date} />
-              ))}
-            </div>
-          </CalendarContextMenu>
-          <CalendarDayDndOverlay />
-        </CalendarDayDndProvider>
-      </div>
-    </div>
-  );
-});
-CalendarRangeView.displayName = "CalendarRangeView";
+import { addDays, CalendarView, getRangeDays } from "@illostack/react-calendar";
+import { CalendarDaysViewTemplate } from "@illostack/react-calendar-day";
 
 const VIEW_ID = "range";
 type CalendarRangeMeta = {
@@ -83,7 +17,7 @@ const view: CalendarView<
   CalendarRangeConfiguration
 > = {
   id: VIEW_ID,
-  content: CalendarRangeView,
+  content: CalendarDaysViewTemplate,
   viewDatesFn(date) {
     return getRangeDays(date, this.meta.days).map((date) => ({
       date,
