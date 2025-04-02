9ccb579698608fdf37c3dfa8f2ae0b5b8aaf0e94
illodev <ajp2010@hotmail.es>
2025-04-03 00:23:31 +0200

    feat: export calendar event card component from index

--- packages/react-calendar-ui/src/index.ts
+++ packages/react-calendar-ui/src/index.ts
@@ -12,12 +12,14 @@ export {
 } from "lucide-react";
 
 export * from "./components/button";
+export * from "./components/calendar-event-card";
 export * from "./components/context-menu";
 export * from "./components/form";
 export * from "./components/input";
 export * from "./components/label";
 export * from "./components/radio-group";
 export * from "./components/sonner";
+
 export * from "./hooks/use-dialog";
 export * from "./hooks/use-sheet";
 
