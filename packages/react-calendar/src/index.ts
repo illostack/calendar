86fed1e2817aa95a73eae5a58ee427c0d53bc979
illodev <ajp2010@hotmail.es>
2025-04-03 00:25:38 +0200

    feat: add hooks for date fragments and date checking to react-calendar

--- packages/react-calendar/src/index.ts
+++ packages/react-calendar/src/index.ts
@@ -3,10 +3,11 @@ export * from "./components/calendar-event-card";
 export * from "./components/calendar-time-indicator";
 
 export * from "./lib/calendar";
-export * from "./lib/position";
 export * from "./lib/time";
 export * from "./lib/utils";
 
+export * from "./hooks/use-date-fragments";
+export * from "./hooks/use-is-in-date";
 export * from "./hooks/use-react-calendar";
 
 export * from "./types";
