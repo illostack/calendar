fb47491ac24a4dae028eef02b41c9f593469ae6c
illodev <ajp2010@hotmail.es>
2025-04-03 00:20:38 +0200

    refactor: simplify ESLint configuration by using nextJsConfig

--- apps/demo/eslint.config.mjs
+++ apps/demo/eslint.config.mjs
@@ -1,38 +1,4 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { nextJsConfig } from "@illostack/eslint-config/next-js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "react/jsx-no-literals": "off",
-      "no-restricted-imports": [
-        "error",
-        {
-          name: "next/link",
-          message: "Please import from `@/i18n/routing` instead."
-        },
-        {
-          name: "next/navigation",
-          importNames: [
-            "redirect",
-            "permanentRedirect",
-            "useRouter",
-            "usePathname"
-          ],
-          message: "Please import from `@/i18n/routing` instead."
-        }
-      ]
-    }
-  }
-];
-
-export default eslintConfig;
+/** @type {import("eslint").Linter.Config} */
+export default nextJsConfig;
